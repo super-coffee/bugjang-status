6352470645cd7a3f4217022c7264cda44cbfdad3
Xiao_Jin <742905606@qq.com>
2020-05-17 21:01:26 +0800

    update README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bugjang-status
 Intuitive Mojang Services Status
+
+Vue + axios + Bootstrap4 + toastr
+
+TRY NOW: <https://bugjang.uuuu.work/>
--- index.js
+++ index.js
@@ -11,19 +11,33 @@ var bugjangStatus = new Vue({
             green: `${baseStyle} bg-success`,
         },
     },
-    mounted() {
-        axios({
-            url: "https://status.mojang.com/check",
-            method: "get",
-        })
-            .then(function (response) {
-                bugjangStatus.servicesList = response.data;
-            })
-            .catch(function (error) {
-                bugjangStatus.checkApiAlive = false;
-            })
-            .then(function () {
-                bugjangStatus.loading = false;
-            });
+    created() {
+        configToastr();
+        getStatus();
     },
 });
+
+function getStatus () {
+    axios({
+        url: "https://status.mojang.com/check",
+        method: "get",
+    })
+        .then(function (response) {
+            bugjangStatus.servicesList = response.data;
+            throw new Error("a error for debug, you can remove this");
+            toastr.success("Loaded status");
+        })
+        .catch(function (error) {
+            bugjangStatus.checkApiAlive = false;
+            toastr.error("Unable to get status");
+        })
+        .then(function () {
+            bugjangStatus.loading = false;
+        });
+}
+
+function configToastr () {
+    toastr.options = {
+        positionClass: "toast-bottom-right",
+    };
+}
